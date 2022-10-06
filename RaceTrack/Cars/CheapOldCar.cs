ecc4c65bdbb84664b9f0d2b2595fa03f8de1e7ff
Hexiphix <jmolenda2002@gmail.com>
2022-10-05 19:26:30 -0500

    A8 Midterm Racecar Assignment

---
+++ RaceTrack/Cars/CheapOldCar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class CheapOldCar : RaceCar
+    {
+        public CheapOldCar()
+        {
+            Name = "Cheap Old Car";
+            TopSpeed = 70;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name}, to everyone's suprise, actually manages to start");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} trys to stop, and the brakes don't give out!");
+            base.Brake();
+        }
+
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name} engine stops as if relieved to be done");
+        }
+    }
+}
